1a4f42f2aecaa9f6437c5d7444aebc59ea5f5594
Jasper <jappe_1999@outlook.com>
2016-09-24 15:55:19 +0200

    filter out no-ascii chars

--- tests/bs.py
+++ tests/bs.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import re
+import re, urllib
 import requests as r
 from bs4 import BeautifulSoup
+from urlparse import urlparse as parser
 
-url      = 'http://wikipedia.org'
+url      = 'http://dmoz.com/'
+uri      = parser(url)
+domain   = '{}://{}/'.format(uri.scheme, uri.netloc)
 response = r.get(url)
 soup     = BeautifulSoup(response.text, 'html.parser')
 results  = []
 
 for link in soup('a'):
     #anchor  = []
-    href = re.sub("^[:]?[\/]{2}", "http://", link.get('href'))
+    href = str(link.get('href'))
+    href = re.sub("^[:]?[\/]{2}", "http://", href)
+    href = re.sub("^[\/]{1}", domain, href)
+
+    #Try this and otherwise the link is False
     try:
-        title = link.string if not link.string == None else ''
-    except:
-        title = ''
+        title = link.string.encode('iso-8859-1') if not link.string == None else 'None'
+        title = link.string.encode('utf-8') if not link.string == None else 'None'
+    except UnicodeEncodeError or UnicodeDecodeError:
+        continue
 
     print '{}, {}\n'.format(href, title)
     '''anchor.append(href)
