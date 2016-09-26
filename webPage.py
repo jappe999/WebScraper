c16260bfdc6554c17eb51e6847a28fbd683a75b6
Jasper <jappe_1999@outlook.com>
2016-09-26 17:53:08 +0200

    Python 3

--- webPage.py
+++ webPage.py
@@ -21,7 +21,7 @@ class webPage(object):
         url = re.sub("^[:]?[\/]{2}", "http://", url) # If url starts with :// or // replace it with http://
         url = re.sub("^[\/]{1}", domain, url) # If url starts with a single slash replace it with the domain
         try:
-            response = urllib.request.urlopen(url).read()
+            response = urllib.request.urlopen(url).read().decode('utf-8')
         except Exception as e:
             pass
         return response
