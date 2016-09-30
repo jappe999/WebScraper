173402cab64c4d7d053b8735f95cc6b8d35c019c
Jasper <jappe_1999@outlook.com>
2016-09-30 14:41:13 +0200

    Added setup.py

--- setup.py
+++ setup.py
@@ -6,20 +6,26 @@ def main():
     for item in pipInstalls:
         try:
             try:
-                command = 'python -m pip install', item
-                os.system(command)
+                command = 'python -m pip install {}'.format(item)
+                system(command)
             except:
                 pass
 
             try:
-                command = 'python3 -m pip install', item
-                os.system(command)
+                command = 'python3 -m pip install {}'.format(item)
+                system(command)
             except:
                 pass
 
             try:
-                command = 'py -m pip install', item
-                os.system(command)
+                command = 'py -m pip install {}'.format(item)
+                system(command)
+            except:
+                pass
+
+            try:
+                command = 'sudo python3 -m pip install {}'.format(item)
+                system(command)
             except:
                 pass
 
