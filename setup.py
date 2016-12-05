60da3c98d0baa03628b7dc7d24cf7cfe28a4a9b8
Jasper <jappe_1999@outlook.com>
2016-12-05 15:32:21 +0100

    Added setup file

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 
 setup(name='WebScraper',
       version='2.0.0',
-      description='WebScraper, THE way to Scrap the Web',
-      author='Mitch Rob, Jasper Van Der Linden, Erik Baalbergen',
+      description='WebScraper, THE way to scrape the Web',
+      author='Mitch Rob, Jasper van der Linden & Erik Baalbergen',
       author_email='jappe999@github.com',
       url='http://github.com/jappe999/WebScrapper',
       install_requires={
