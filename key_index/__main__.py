775b030861419bb3e99776501066433e67a1b5d1
jappe999 <jappe999@users.noreply.github.com>
2016-12-15 21:25:25 +0100

    Bug fixed and added comments
    If the URL is not found on the server it does not update the servers' column indexed in table queue.

--- key_index/__main__.py
+++ key_index/__main__.py
@@ -10,25 +10,22 @@ def main(number):
 
         for line in response:
             try:
-                database.update(line[0])
-                url = re.sub('^(http://|https://)(www\.)?', '', line[1])
+                url = re.sub('^(http://|https://)(www\.)?', '', line[1]) # Line[1] is the URL from table queue.
                 url = re.sub('(/)$', '', url)
                 print(url)
                 file = '/home/user/WebScraper/Data/Meta/' + url + '/main.meta'
+                
+                keywords = getKeywords(file)
+                print(getTitle(file))
+                database.addKeywords(line[0], keywords) # Line[0] is the ID of the URL in table queue.
+                database.update(line[0])
             except Exception as e:
                 print(e)
                 continue
-
-            try:
-                keywords = getKeywords(file)
-                print(getTitle(file))
-                database.addKeywords(line[0], keywords)
-            except Exception as e:
-                print(e)
-#        break
-    database.close()
+    database.close() # Close database if loop stopped.
 
 
 
 if __name__ == '__main__':
-    main(100)
+    main(100) # Call main definition with the parameter
+              # that represents the amount of URL's proccessed each loop.
