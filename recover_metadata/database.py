ce8aeceb7735ae79cecebdab7ac5171d177d7487
Jasper <jappe_1999@outlook.com>
2017-01-07 12:48:13 +0100

    Added recover_metadata directory

---
+++ recover_metadata/database.py
@@ -0,0 +1,77 @@
+import pymysql, re, time
+from threading import Thread
+from colorama import init, Fore, Back, Style
+from urllib.parse import quote, unquote
+from sys import exit
+
+class Database(object):
+    def __init__(self, user, password, database):
+        try:
+            init()
+            self.db = pymysql.connect (
+                        host="127.0.0.1",
+                        port=3306,
+                        user=user,
+                        password=password,
+                        db=database
+                      )
+            self.cursor = self.db.cursor()
+        except Exception as e:
+            print(Fore.RED + "BETA error 0x1:")
+            print(e)
+            print(Style.RESET_ALL)
+            exit()
+
+    def getQueue(self, numberOfLinks=10):
+        self.cursor.execute("SELECT url FROM queue WHERE recovery = '0' LIMIT " + str(numberOfLinks) + ";")
+        result = self.cursor.fetchall()
+        response = []
+        self.remove(result)
+        for i in range(len(result)):
+            response.append(unquote(result[i][0]))
+            response[i] = re.sub(r'(\\)*$', '', response[i])
+        return response
+
+
+    def writeToDb(self, url):
+        try:
+            self.cursor.execute("INSERT INTO queue (url, recovery) VALUES ('" + self.escapeURL(url) + "', '1');")
+            self.db.commit()
+        except Exception as e:
+            print(Fore.RED + "BETA error 0x2:")
+            print(e)
+            print(Style.RESET_ALL)
+
+    def setQueue(self, obj):
+        for url in obj:
+            t = Thread(target=self.writeToDb(url))
+            t.daemon = True
+            t.start()
+        return True
+
+    def escapeURL(self, url):
+        return re.sub(r'(\\)*$', '', url)
+
+    def updateQueue(self, url):
+        try:
+            self.cursor.execute("UPDATE queue SET visited='1' WHERE url = '" + self.escapeURL(url) + "';")
+            self.db.commit()
+        except Exception as e:
+            print(Fore.RED + "BETA error 0x3:")
+            print(e)
+            print(Style.RESET_ALL)
+
+    def remove(self, obj):
+        for line in obj:
+            url = line[0]
+            t = Thread(target=self.updateQueue(url))
+            t.daemon = True
+            t.start()
+
+
+    def execute(self, command):
+        self.cursor.execute(command)
+        self.db.commit()
+
+    def close(self):
+        self.db.close()
